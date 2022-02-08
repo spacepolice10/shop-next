d7d33b069aa43036ad457149dcb109fa6d88105f
spacepolice10 <savemyrights0101@gmail.com>
2022-02-08 21:18:40 +0300

    feat: add search bar interface

--- pages/testData.js
+++ pages/testData.js
@@ -1,11 +1,24 @@
 const testData = {
 	items: {
+		accessories: [
+			{
+				id: 1992,
+				name: 'Glasses',
+				price: 12,
+				size: 'One size',
+				color: 'Black',
+				description: 'Some text about this awesome glasses from California, bla-bla-bla',
+				comments: [],
+				score: 3.5,
+				badge: 'Hot',
+			}
+		],
 		clothes: [
 			{
 				id: 2020,
 				name: 'Jacket',
-				price: 1200,
-				size: 'XS',
+				price: 68,
+				size: 'XL',
 				color: 'Black',
 				description: 'Some text about this awesome jacket from nowhere, bla-bla-bla',
 				comments: [],
@@ -15,12 +28,23 @@ const testData = {
 			{
 				id: 4040,
 				name: 'Shoes',
-				price: 700,
+				price: 70,
 				size: 'S',
 				color: 'Red',
 				description: 'Shoes,shoes,shoes,shoes',
 				comments: [],
 				score: 3.9,
+				badge: 'Season',
+			},
+			{
+				id: 6060,
+				name: 'Bra',
+				price: 44,
+				size: 'XS',
+				color: 'Blue',
+				description: 'For the gorgeous one',
+				comments: [],
+				score: 4.9,
 				badge: 'Season',
 			}
 		]
@@ -30,6 +54,13 @@ const testData = {
 		mine: [],
 		subscriptions: [],
 	},
+	categories: [
+		{
+			name: 'Beach season',
+			picture: '',
+			query: ''
+		}
+	],
 	blog: {},
 }
 
