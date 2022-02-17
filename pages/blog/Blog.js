9c5aab86a05058e5b321beb5ceb3154c84523f98
spacepolice10 <savemyrights0101@gmail.com>
2022-02-17 12:57:57 +0300

    feat: implement basic blog-component

--- pages/blog/Blog.js
+++ pages/blog/Blog.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+import testData from "../testData";
+
 export default function Blog () {
+    const lastArticles = testData.blog.map(article => article.title)
+    useEffect(() => {
+        
+    })
     return (
         <div>
-            
+            { lastArticles }
         </div>
     )
 }
