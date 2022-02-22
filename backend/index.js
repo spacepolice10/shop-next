e0723579bd2cce3b39b3afad2c1491cb3f802eb7
spacepolice10 <savemyrights0101@gmail.com>
2022-02-22 17:45:10 +0300

    feat: set up sequelize launching

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,18 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
 
+const PORT = process.env.PORT || 5000
+
 const app = express()
 
-app.listen(5000, () => console.log('hey'))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
